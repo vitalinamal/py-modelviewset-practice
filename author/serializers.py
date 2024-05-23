c89a4fcbb6119203768a017ee16ccb6ae81a64ec
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-05-23 19:59:02 +0200

    solution

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    first_name = serializers.CharField(max_length=64)
+    last_name = serializers.CharField(max_length=64)
+    pseudonym = serializers.CharField(required=False, max_length=64)
+    age = serializers.IntegerField()
+    retired = serializers.BooleanField()
+
+    class Meta:
+        model = Author
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired"
+        ]
--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,24 @@
-# Create your urls here
+from django.urls import path, include
+from author.views import AuthorViewSet
 
-urlpatterns = []
+# from rest_framework import routers
+#
+# router = routers.DefaultRouter()
+# router.register('authors', AuthorViewSet, basename='author')
+author_list = AuthorViewSet.as_view(actions={"get": "list", "post": "create"})
+author_detail = AuthorViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "delete": "destroy",
+        "patch": "partial_update",
+    }
+)
+
+urlpatterns = [
+    path("authors/", author_list, name="manage-list"),
+    path("authors/<int:pk>/", author_detail, name="manage-detail"),
+    # path('', include(router.urls)),
+]
+
+app_name = "author"
